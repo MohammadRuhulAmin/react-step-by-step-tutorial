82e4591f1ee9a7cc63c9e6cb0d128b67af4084dc
Md Ruhul Amin <ra.01403288711@gmail.com>
2021-09-20 21:46:19 +0600

    Tutorial-8 jsx

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Greet from './components/Greet'
 import { ExpGreet } from './components/ExportGreet';
 import Welcome from './components/Welcome';
+import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
       <Greet/>
       <ExpGreet/>
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
+
+
+
 
 
 
---
+++ src/components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello =  ()=>{
+    return (
+       <div>
+            <h1>Hellow Ruhul From JSX</h1>
+       </div>
+    )
+
+}
+export default Hello 

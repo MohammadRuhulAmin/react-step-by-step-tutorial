183bb0917ac54fe118adf4510421886976db1ca0
Md Ruhul Amin <ra.01403288711@gmail.com>
2021-09-20 21:32:41 +0600

    Tutorial 6,7

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import { ExpGreet } from './components/ExportGreet';
-
+import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
       <Greet/>
       <ExpGreet/>
+      <Welcome/>
     </div>
   );
 }
+
+
 
 export default App;
